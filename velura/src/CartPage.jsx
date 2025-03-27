8505bb9219cb801ddc0fe09c2c99838643443105
Lochana nimsara <136056300+laserXnext@users.noreply.github.com>
2025-03-27 20:28:50 +0530

    Update CartPage.jsx

--- velura/src/CartPage.jsx
+++ velura/src/CartPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import Cart from './components/Cart';
 
 const CartPage = () => {
