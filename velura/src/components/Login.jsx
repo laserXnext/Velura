8fae3a61a6f3431e4cbb7af2dd1bc1eb0abd92bf
Lochana nimsara <136056300+laserXnext@users.noreply.github.com>
2025-03-27 20:25:37 +0530

    Update Login.jsx

--- velura/src/components/Login.jsx
+++ velura/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import '../CSS/Login.css';
+import '../CSS/login.css';
 
 const Login = () => {
   const [formData, setFormData] = useState({ username: '', password: '' });
